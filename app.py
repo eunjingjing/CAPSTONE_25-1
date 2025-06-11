f5b69623ab1ff3d4eac92ac2f33715e0b2046064
Jiwon <aebeole03@gmail.com>
2025-06-12 02:06:14 +0900

    HTML 라우팅 및 static 경로 수정, css/js 분리 적용

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# 메인 페이지
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# 로그인 페이지
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# 회원가입 페이지
+@app.route('/sign-in')
+def sign_in():
+    return render_template('sign_in.html')
+
+# 비밀번호 찾기 페이지
+@app.route('/find-password')
+def find_password():
+    return render_template('find_password.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
