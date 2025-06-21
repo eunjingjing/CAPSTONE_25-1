5a224a5bd78868e7016d9e4920b582901fe700ee
이은진 <149948792+eunjingjing@users.noreply.github.com>
2025-06-21 18:37:44 +0900

    모델 경로 수정

--- recommend.py
+++ recommend.py
@@ -401,8 +401,7 @@ def recommend_for_image(image_path: str, handedness: str, user_overrides: dict):
         # 1. 모델 로딩
         MODEL_PATH = os.path.join(BASE_DIR, "models/weights/best.pt")
         print(f"📦 모델 경로 확인: {MODEL_PATH}")
-        # model = YOLO(MODEL_PATH)
-        model = YOLO("yolov8s.pt")
+        model = YOLO(MODEL_PATH)
         print("✅ 모델 로딩 완료")
 
         # 2. 이미지 로딩
